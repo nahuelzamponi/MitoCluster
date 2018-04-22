da37d0e797b5a3a1a2042113b3d9ba346e63b7b4
nahuelzamponi <38571044+nahuelzamponi@users.noreply.github.com>
2018-04-21 23:11:40 -0400

    Create CCDF.m

---
+++ CCDF.m
@@ -0,0 +1,33 @@
+
+
+
+
+image='path to image';
+clear c m n cb puntos L num LL g
+
+
+c=imread(image);
+[m n]=size(c);
+
+th=graythresh(c); %threshold
+
+cb=im2bw(c, th);
+puntos = bwmorph(cb,'skel', Inf);
+spy(puntos);
+pause;
+
+
+[L,num]=bwlabel(puntos,8);
+[m,n]=size(L);
+
+LL=reshape(L,m*n,1);
+%display(['#clusters ', num2str(num)])
+
+for ii=1:num
+	g(ii)=length(find(LL == ii));
+end
+
+
+figure(1)
+[bins, H]=cumhist2(g, max(g));
+loglog(H, bins, '-ok');
